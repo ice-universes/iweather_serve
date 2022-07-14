70b4fd54c9b967035796b6094019620915add358
ARCTURUS <1638330246@qq.com>
2022-07-14 14:36:42 +0800

    feat: 相关配置从环境变量中读取

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,34 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from '@app/users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+// 配置文件
+import databaseConfig from '@src/config/database.config';
+import authConfig from '@src/config/auth.config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017', {
-      dbName: 'user',
+    ConfigModule.forRoot({
+      load: [databaseConfig, authConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const db = configService.get<IDataBaseConfig>('database');
+
+        const { host, port, dbName, userName, password } = db;
+
+        let admin = '';
+        if (userName && password) {
+          admin = `${userName}:${password}@`;
+        }
+
+        return {
+          uri: `mongodb://${admin}${host}:${port}/${dbName}`,
+        };
+      },
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
