90edc5f7dbb04d07181768025dd0c630803d6ecb
ARCTURUS <1638330246@qq.com>
2022-07-14 23:44:06 +0800

    feat: 修改 schema

--- src/app/mongo/favorites.schema.ts
+++ src/app/mongo/favorites.schema.ts
@@ -1,12 +1,16 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Schema as MongooseSchema } from 'mongoose';
 import { Document } from 'mongoose';
+import { Location } from './calendar.schema';
+
+class Favorites_ extends Location {}
 
 export type FavoritesDocument = Favorites & Document;
 
 @Schema()
 export class Favorites extends Document {
-  @Prop({ required: true })
-  list: Array<IFavorites>;
+  @Prop({ type: MongooseSchema.Types.Array, required: true })
+  list: Favorites_[];
 }
 
 export const FavoritesSchema = SchemaFactory.createForClass(Favorites);
