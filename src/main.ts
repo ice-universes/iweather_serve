2ecdc7f96eff4bf26470c6b4bb28b30a82966d92
ARCTURUS <1638330246@qq.com>
2022-07-15 21:45:18 +0800

    feat: 修改端口逻辑

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from '@app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(parseInt(process.env.PORT) ?? 3000);
+  const port = parseInt(process.env.PORT);
+
+  await app.listen(isNaN(port) ? 3000 : port);
 }
 
 bootstrap();
